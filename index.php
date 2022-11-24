3c5c1f9633ca3750b01187e53626e37e5939aef7
Gulevskii Ivan <ithirteengw@gmail.com>
2022-11-24 16:42:48 +0700

    added some files for learning php

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
 <?php
-echo "hello world!";
+header('Content-type: application/json');
+$x = '{ 
+    "message": "hello"
+    "sender" : {
+        "id": 777,
+        "name": "Ivan",
+        "lines": [
+            1, 2, 3, 4
+        ]
+    } 
+}';
+
+$message = array(
+    'message' => "hello",
+    'sender' => [
+        'name' => "Ivan",
+        'lastname' => "Gulevskii",
+        'id' => 76678
+    ]
+);
+
+$message['sender']['orders'] = [1, 2, 3];
+
+$orderNames = ["sushi", "pizza", "bread", "shamov egor"];
+
+foreach ($orderNames as $key => $value) {
+    $message['sender']['orders'][$key] = [];
+    $message['sender']['orders'][$key]['id'] = $key;
+    $message['sender']['orders'][$key]['name'] = $value;
+}
+
+echo json_encode($message);
+
+
