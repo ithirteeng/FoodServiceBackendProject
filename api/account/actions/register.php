5a7a2a1b60b629c10da7348ef9ddc255b87ee843
shift-lab-gulevsky <ithirteengw@gmail.com>
2022-12-01 22:16:23 +0700

    1)added token on registration

--- api/account/actions/register.php
+++ api/account/actions/register.php
@@ -12,18 +12,28 @@ function checkRequestMethods($method): void
 
 function postData($requestData): void
 {
-    $fullName = $requestData->body->fullName;
-    $password = $requestData->body->password;
-    $address = $requestData->body->address;
+    global $link;
     $email = $requestData->body->email;
-    $birthdate = $requestData->body->birthDate;
-    $gender = $requestData->body->gender;
-    $phoneNumber = $requestData->body->phoneNumber;
-
-    if (getRegistrationValidationResult($requestData)) {
-        echo "valid";
+    if (pg_fetch_assoc(
+        pg_query($link, "select email from users where email = '$email'")
+    )) {
+        setHttpStatus("409", "User already exists!");
     } else {
-        echo "\ninvalid";
+        $fullName = $requestData->body->fullName;
+        $password = $requestData->body->password;
+        $address = $requestData->body->address;
+        $birthdate = $requestData->body->birthDate;
+        $gender = $requestData->body->gender;
+        $phoneNumber = $requestData->body->phoneNumber;
+        $hashPassword = hash("sha1", $password);
+        if (getRegistrationValidationResult($requestData)) {
+            pg_query($link, "insert into users (fullname, email, address, birthdate, phonenumber, password, gender)
+                                    values ('$fullName', '$email', '$address', '$birthdate', '$phoneNumber', '$hashPassword', '$gender')");
+            $newToken = createToken($email);
+            if (checkIfTokenInBlackList($newToken)) {
+                pg_query($link, "delete from token_blacklist where value = '$newToken'");
+            }
+            echo json_encode(["token" => $newToken]);
+        }
     }
-
 }
